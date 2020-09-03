115288f47d2567c77a3f849dd74292775c85410f
Akash Kundu <akash.kundu@compass.com>
2020-09-02 21:22:49 -0700

    updated server

--- server.go
+++ server.go
@@ -1,15 +1,33 @@
 package main
+ 
+  import (
+      "fmt"
+      "log"
+      "net/http"
+      "os"
+  )
+ 
+ func main() {
+     http.HandleFunc("/", handler)
+     fmt.Println("listening...")
+     err := http.ListenAndServe(GetPort(), nil)
+     if err != nil {
+         log.Fatal("ListenAndServe: ", err)
+     }
+ }
 
-import (
-    "fmt"
-    "net/http"
-)
+ func handler(w http.ResponseWriter, r *http.Request) {
+     fmt.Fprintf(w, "Hello. This is our first Go web app on Heroku!")
+ }
 
-func helloWorld(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hello Go World")
-}
+ // Get the Port from the environment so we can run on Heroku
+ func GetPort() string {
+     var port = os.Getenv("PORT")
+     // Set a default port if there is nothing in the environment
+     if port == "" {
+         port = "4747"
+         fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+     }
+     return ":" + port
+ }
 
-func main() {
-    http.HandleFunc("/", helloWorld)
-    http.ListenAndServe(":8080", nil)
-}
