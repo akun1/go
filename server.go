195de3dcd7e7ad7c5c78dce3ef3d3bf95a934e2d
Akash Kundu <akash.kundu@compass.com>
2020-09-03 15:58:29 -0700

    added another route

--- server.go
+++ server.go
@@ -1,33 +1,37 @@
 package main
- 
-  import (
-      "fmt"
-      "log"
-      "net/http"
-      "os"
-  )
- 
- func main() {
-     http.HandleFunc("/", handler)
-     fmt.Println("listening...")
-     err := http.ListenAndServe(GetPort(), nil)
-     if err != nil {
-         log.Fatal("ListenAndServe: ", err)
-     }
- }
 
- func handler(w http.ResponseWriter, r *http.Request) {
-     fmt.Fprintf(w, "Hello. This is our first Go web app on Heroku!")
- }
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
- // Get the Port from the environment so we can run on Heroku
- func GetPort() string {
-     var port = os.Getenv("PORT")
-     // Set a default port if there is nothing in the environment
-     if port == "" {
-         port = "4747"
-         fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
-     }
-     return ":" + port
- }
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/message", message)
+	fmt.Println("listening...")
+	err := http.ListenAndServe(GetPort(), nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello. This is our first Go web app on Heroku!")
+}
+
+func message(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "this is a secret message 🙂")
+}
+
+// Get the Port from the environment so we can run on Heroku
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "4747"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
+}
